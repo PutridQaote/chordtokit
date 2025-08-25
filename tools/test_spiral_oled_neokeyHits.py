2ba9567f9eea1d6e55f2fb44dd8cfbdd7909d42e
PutridQaote <m.tyler.haas@gmail.com>
2025-08-24 18:29:43 -0700

    oled i2c address fixed

--- tools/test_spiral_oled_neokeyHits.py
+++ tools/test_spiral_oled_neokeyHits.py
@@ -23,7 +23,7 @@ NEOKEY_BRIGHT    = 0.4
 # OLED CONSTANTS
 #----------------------//
 
-OLED_ADDR      = 0x3C
+OLED_ADDR      = 0x3D
 OLED_WIDTH     = 128
 OLED_HEIGHT    = 64
 OLED_RESET_PIN = 4 # wtf is this
